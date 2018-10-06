02f3c82291d5abc31c4da05d10aa2ce2d498817d
jhuzef <josephgonzales034@gmail.com>
2018-10-05 19:17:35 -0700

    Feature: Read from the CSV and populate the appropriate properties of the record object

--- Public/readCSV.php
+++ Public/readCSV.php
@@ -1,6 +1,26 @@
 <?php
 
 
+    class csv {
+        static public function getRecords($filename) {
+            $file = fopen($filename,"r");
+            $fieldNames = array();
+            $count = 0;
+            while(! feof($file))
+            {
+                $record = fgetcsv($file);
+                if($count == 0) {
+                    $fieldNames = $record;
+                } else {
+                    $records[] = recordFactory::create($fieldNames, $record);
+                }
+                $count++;
+            }
+            fclose($file);
+            return $records;
+        }
+    }
+
     class record {
         public function __construct(Array $fieldNames = null, $values = null )
         {
