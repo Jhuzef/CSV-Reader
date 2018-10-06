ab069390a59700a854882907a1dd2229cc7edefb
jhuzef <josephgonzales034@gmail.com>
2018-10-05 19:16:17 -0700

    Feature: Added a factory class for the record object

--- Public/readCSV.php
+++ Public/readCSV.php
@@ -18,5 +18,15 @@
         }
     }
 
+    class recordFactory
+    {
+        public static function create(Array $fieldNames = null, Array $values = null)
+        {
+            $record = new record($fieldNames, $values);
+            return $record;
+        }
+    }
+
+
 
 ?>
