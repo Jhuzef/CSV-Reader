08a9e47fd99e4347e99081416c9c42a89e8da67c
jhuzef <josephgonzales034@gmail.com>
2018-10-05 19:31:38 -0700

    Feature: Added an index.php

---
+++ Public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CSV Reader</title>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php
+    include("readCSV.php");
+?>
+
+</body>
+</html>
