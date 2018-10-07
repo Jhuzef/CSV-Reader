4a9ff2ca25ab85cd47627a99ced2593d1af5b1b6
jhuzef <josephgonzales034@gmail.com>
2018-10-07 13:34:22 -0700

    Feature: Added a html class to dynamically generate a table.

--- Public/readCSV.php
+++ Public/readCSV.php
@@ -5,7 +5,62 @@
     class main  {
         static public function start($filename) {
             $records = csv::getRecords($filename);
+            $html = html::createTable($records);
+            print $html;
         }
+    }
+
+    class html {
+
+        public static function createTable($records) {
+            return "<table class=\"table table-striped\">" . html::createColumns($records[0]) . html::createRows($records) . "</table>";
+
+        }
+
+        public static function tableBody($data) {
+            return "<tbody>" . $data . "</tbody>";
+        }
+
+
+        public static function tableHeader($data) {
+            return "<thead>" . $data . "</thead>";
+        }
+
+        public static function columnHeader($data) {
+            return "<th>" . $data . "</th>";
+        }
+
+
+        public static function tableRow($data) {
+            return "<tr>" . $data . "</tr>";
+        }
+
+        public static function tableData($data) {
+            return "<td>" . $data . "</td>";
+        }
+
+        public static function createColumns($records) {
+            $html = "";
+            foreach ($records as $key => $value) {
+                $html .= html::columnHeader($key);
+            }
+
+            return html::tableHeader($html);
+        }
+
+        public static function createRows($records) {
+            $html = "";
+            for ($i=0; $i < count($records); $i++) {
+                foreach ($records[$i] as $key => $value) {
+                    $html .= html::tableData($value);
+                }
+                $html = html::tableRow($html);
+            }
+
+            return html::tableBody($html);
+        }
+
+
     }
 
     class csv {
@@ -24,6 +79,7 @@
                 $count++;
             }
             fclose($file);
+
             return $records;
         }
     }
